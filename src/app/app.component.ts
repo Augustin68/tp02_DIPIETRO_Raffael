4064650e4bc6b58be55e0ffc34e27afe6e09d5bb
Raffael Di Pietro <maxou4170@gmail.com>
2024-02-01 16:39:59 +0100

    add prettier and eslint nice working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,29 @@
-import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
-import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER, TuiButtonModule } from "@taiga-ui/core";
+import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
+import {
+  TuiRootModule,
+  TuiDialogModule,
+  TuiAlertModule,
+  TUI_SANITIZER,
+} from '@taiga-ui/core';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TuiRootModule, TuiDialogModule, TuiAlertModule, TuiButtonModule],
+  imports: [
+    RouterOutlet,
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
+    NavbarComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-    providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}]
+  providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
 })
 export class AppComponent {
-  title = 'tp02';
+  title = 'patate';
 }
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TuiButtonModule } from '@taiga-ui/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [ CommonModule, TuiButtonModule,
+  ],
+  templateUrl: './navbar.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavbarComponent { }
